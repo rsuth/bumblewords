bd56f1514a92cb2cf374f1d219202f436a0b42c2
Rick Sutherland <suthe.rick@gmail.com>
2021-04-26 18:07:06 -0700

    add dark mode for other pages

---
+++ public/leaderboard.js
@@ -0,0 +1,37 @@
+function setCookie(name, value, exp, path = '/') {
+    const expires = exp.toUTCString();
+    document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=' + path + '; SameSite=Lax';
+};
+
+function getCookie(name) {
+    return document.cookie.split('; ').reduce((r, v) => {
+        const parts = v.split('=');
+        return parts[0] === name ? decodeURIComponent(parts[1]) : r
+    }, '');
+};
+
+var darkMode = getCookie('darkMode') ? parseInt(getCookie('darkMode')) : 0;
+
+function toggleDarkMode(){
+    let midnight = new Date();
+    midnight.setHours(23,59,59,0);
+    
+    darkMode ^= true;
+    setCookie('darkMode', darkMode, midnight);
+
+    if(darkMode){
+        document.body.classList.add('darkMode');
+        document.querySelector('#darkmode-toggle').textContent = 'light mode';
+    }else {
+        document.body.classList.remove('darkMode');
+        document.querySelector('#darkmode-toggle').textContent = 'dark mode';
+    } 
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    if(darkMode !== 0){
+        document.body.classList.add('darkMode');
+        document.querySelector('#darkmode-toggle').textContent = 'light mode';
+    }
+    document.querySelector('#darkmode-toggle').addEventListener('click', toggleDarkMode);
+})
